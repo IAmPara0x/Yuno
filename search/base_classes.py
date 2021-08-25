f3a93300aa680b675d6e5e9b20b2bbdbc32eed0b
IAmParadox <confusedstudent13@gmail.com>
2021-08-25 20:59:10 +0530

    reworked files structure

---
+++ search/base_classes.py
@@ -0,0 +1,158 @@
+
+import numpy as np
+from enum import Enum
+from typing import NamedTuple, List, Callable, Any, Dict, Union
+from abc import ABCMeta, abstractmethod
+
+from model import Model
+from config import Config
+
+
+class Genre(NamedTuple):
+  uid: int
+  name: str
+
+
+class Tag(NamedTuple):
+  uid: int
+  name: str
+  description: str
+  embedding: np.ndarray
+  category_uid: int
+
+
+class TagCategory(NamedTuple):
+  uid: int
+  name: str
+  description: str
+  tags_uid: List[int]
+
+  def tag_pos(self, tag_uid) -> int:
+    return self.tags_uid.index(tag_uid)
+
+
+class Anime(NamedTuple):
+  uid: int
+  name: str
+  genres_uid: List[int]
+  tags_uid: List[int]
+  tags_score: List[int]
+
+
+class Query(NamedTuple):
+  text: str
+  embedding: np.ndarray
+
+
+class SearchResult(NamedTuple):
+  query: Query
+  result_embeddings: np.ndarray
+  result_indexs: np.ndarray
+  scores: np.ndarray
+  anime_infos: List[Anime]
+
+
+class SearchBase(NamedTuple):
+  MODEL: Model
+  INDEX : Any
+  ALL_ANIME: Dict[int,Anime]
+  ALL_TAGS: Dict[int,Tag]
+  ALL_TAGS_CATEGORY: Dict[int,TagCategory]
+  ALL_GENRE: Dict[int,Genre]
+  EMBEDDINGS: np.ndarray
+  LABELS: np.ndarray
+  TEXTS: List[str]
+
+
+class sort_search:
+  def __init__(self, func: Callable[[ReIndexerBase, SearchResult],SearchResult]) -> None:
+    self.func = func
+
+  def __call__(self, *args, **kwargs) -> SearchResult:
+    search_result = self.func(*args)
+    result_embeddings = self.sort(search_result.scores,search_result.result_embeddings)
+    result_indexs = self.sort(search_result.scores,search_result.result_indexs)
+    anime_infos = self.sort(search_result.scores,search_result.anime_infos)
+
+    return self.new_search_result(search_result,result_embeddings=result_embeddings,
+                                  result_indexs=result_indexs,anime_infos=anime_infos)
+
+  def sort(self, scores: np.ndarray, values: Union[List[Anime],np.ndarray]) -> Union[List[Anime],np.ndarray]:
+    return [value  for _, value in sorted(zip(scores,values),reverse=True)]
+
+  def new_search_result(self, prev_result: SearchResult, **kwargs) -> SearchResult:
+    remaining_fields = set(prev_result._fields) - set(kwargs.keys())
+    kwargs.update({field_name: getattr(prev_result,field_name) for field_name in remaining_fields})
+    return SearchResult(**kwargs)
+
+
+class normalize:
+  def __init__(self, func: Callable[[SearchResult],SearchResult]):
+    self.func = func
+
+  def __call__(self, *args, **kwargs) -> SearchResult:
+    search_result = self.func(*args)
+
+    if kwargs.get("sigmoid") == True:
+      scores = self.normalize_scores(search_result.scores)
+    else:
+      scores = self.rescale_scores(search_result.scores)
+
+    return self.new_search_result(search_result,scores=scores)
+
+  @staticmethod
+  def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+  def normalize_scores(self, scores:np.ndarray) -> np.ndarray:
+    return self.sigmoid(scores)
+
+  def rescale_scores(self, scores:np.ndarray) -> np.ndarray:
+    return scores/np.linalg.norm(scores)
+
+  def new_search_result(self, prev_result: SearchResult, **kwargs) -> SearchResult:
+    remaining_fields = set(prev_result._fields) - set(kwargs.keys())
+    kwargs.update({field_name: getattr(prev_result,field_name) for field_name in remaining_fields})
+    return SearchResult(**kwargs)
+
+
+#NOTE: new_search_result is getting used in 3 classes normalize,sort_search,ReIndexerBase
+
+class ReIndexerBase(metaclass=ABCMeta):
+  def __init__(self,search_base:SearchBase, config:Config) -> None:
+    for name,val in zip(search_base._fields,search_base.__iter__()):
+      setattr(self,name,val)
+
+    config_name = f"{self.name}_config"
+    reindexer_config = getattr(config,config_name)
+
+    for name,val in zip(reindexer_config._fields,reindexer_config.__iter__()):
+      setattr(self,name,val)
+
+  @normalize
+  @sort_search
+  @abstractmethod
+  def __call__(self, search_result:SearchResult) -> SearchResult:
+    pass
+
+  @classmethod
+  def name(cls) -> str:
+    return cls.__name__.lower()
+
+  def new_search_result(self, prev_result: SearchResult, **kwargs) -> SearchResult:
+    remaining_fields = set(prev_result._fields) - set(kwargs.keys())
+    kwargs.update({field_name: getattr(prev_result,field_name) for field_name in remaining_fields})
+    return SearchResult(**kwargs)
+
+
+class ReIndexingPipeline:
+  _reindexer_names: List[str] = []
+
+  @classmethod
+  def add_reindexer(cls, name: str, reindexer: ReIndexerBase) -> None:
+    cls._reindexer_names.append(name)
+    setattr(cls,name,reindexer)
+
+  @classmethod
+  def reindex_all(cls, search_result: SearchResult) -> SearchResult:
+    pass
---
+++ search/model.py
@@ -0,0 +1,72 @@
+from typing import List
+import torch
+import torch.nn as nn
+from transformers import RobertaTokenizerFast, RobertaModel # type: ignore
+
+
+class FCN(nn.Module):
+  def __init__(self, input_dim:int, embedding_dim:int, n_hidden_layers:int, n_hidden_units:List[int], dropout_prob:float):
+    super(FCN, self).__init__()
+
+    assert n_hidden_layers != 0
+    assert len(n_hidden_units) + 1 == n_hidden_layers
+
+    encoder_layers = []
+    for i in range(n_hidden_layers):
+      if i == n_hidden_layers - 1:
+        out_dim = embedding_dim
+        encoder_layers.extend(
+          [
+            nn.Linear(input_dim, out_dim),
+          ])
+        continue
+      else:
+        out_dim = n_hidden_units[i]
+
+      encoder_layers.extend( # type: ignore
+        [
+          nn.Linear(input_dim, out_dim), # type: ignore
+          nn.Tanh(),# type: ignore
+          nn.Dropout(dropout_prob, inplace=False),# type: ignore
+        ]
+      )
+      input_dim = out_dim
+    self.encoder = nn.Sequential(*encoder_layers)
+
+  def forward(self, x_array: torch.Tensor) -> torch.Tensor:
+      return self.encoder(x_array)
+
+
+class EmbeddingModel(nn.Module):
+  def __init__(self, model_name: str) -> None:
+    super().__init__()
+    self.roberta = RobertaModel.from_pretrained(model_name, output_hidden_states=True)
+    self.hid_mix = 5
+    self.feats = self.roberta.pooler.dense.out_features
+    self.embedding_layers = FCN(self.feats, 1280, 4, [896, 1024, 1152], 0.1)
+    self.tanh = nn.Tanh()
+
+  def forward(self, x: torch.Tensor) -> torch.Tensor:
+    outputs = self.roberta(x)
+    hidden_states = outputs[2]
+    hmix = []
+    for i in range(1, self.hid_mix+1):
+      hmix.append(hidden_states[-i][:,0].reshape((-1,1,self.feats)))
+
+    hmix_tensor = torch.cat(hmix,1)
+    pool_tensor = torch.mean(hmix_tensor,1)
+    pool_tensor = self.tanh(pool_tensor)
+    embeddings = self.embedding_layers(pool_tensor)
+    return embeddings
+
+class Model:
+  def __init__(self, model_name:str) -> None:
+    self.device = "cuda" if torch.cuda.is_available() else "cpu"
+    self.embedding_model = EmbeddingModel(model_name).to(self.device)
+    self.embedding_model.eval()
+    self.tokenizer = RobertaTokenizerFast.from_pretrained(model_name)
+
+  def __call__(self,text:str) -> torch.Tensor:
+    tokenized_text = self.tokenizer(text, return_tensors="pt")["input_ids"].to(self.device)
+    with torch.no_grad():
+      return self.embedding_model(tokenized_text).squeeze().cpu().numpy()
