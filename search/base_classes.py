315ced9cf3eef84dd196670cf9265e382e9cfbed
IAmParadox <confusedstudet13@gmail.com>
2021-09-23 08:44:16 +0530

    refractored till knn search

--- search/base_classes.py
+++ search/base_classes.py
@@ -1,9 +1,9 @@
-
+from typing import List, Callable, Any, Dict, Union, Tuple, NewType, Optional
+from dataclasses import dataclass, field
+from returns.maybe import Some,Nothing,Maybe
 from functools import wraps
-from toolz.curried import curry,reduce,map,apply,compose,partial
+from toolz.curried import reduce,map,compose,concat# type: ignore
 import numpy as np
-from enum import Enum
-from typing import NamedTuple, List, Callable, Any, Dict, Union, Tuple
 from abc import ABCMeta, abstractmethod
 
 from .model import Model
@@ -11,95 +11,110 @@ from .config import Config
 from . import utils
 
 
-class Genre(NamedTuple):
-  uid: int
-  name: str
-
-  def __eq__(self, other_genre: object) -> bool:
-    if not isinstance(other_genre, Tag):
-      return NotImplemented
-    return other_genre.uid == self.uid
+GenreId = NewType("GenreId", int)
+TagId = NewType("TagId", int)
+TagCategoryId = NewType("TagCategoryId", int)
+AnimeId = NewType("AnimeId", int)
+Scores = NewType("Scores",np.ndarray)
 
 
-class Tag(NamedTuple):
-  uid: int
-  name: str
-  description: str
-  embedding: np.ndarray
-  category_uid: int
-
-  def __eq__(self, other_tag: object) -> bool:
-    if not isinstance(other_tag, Tag):
-      return NotImplemented
-    return other_tag.uid == self.uid
+@dataclass(init=True,repr=True,eq=True,order=False,frozen=True)
+class Genre:
+  uid: GenreId
+  name: str = field(compare=False)
 
 
-class TagCategory(NamedTuple):
-  uid: int
-  name: str
-  tags_uid: List[int]
-
-  def tag_pos(self, tag_uid) -> int:
-    return self.tags_uid.index(tag_uid)
-
-  def __eq__(self, other_category: object) -> bool:
-    if not isinstance(other_category, TagCategory):
-      return NotImplemented
-    return other_category.uid == self.uid
+@dataclass(init=True,repr=True,eq=True,order=False,frozen=True)
+class Tag:
+  uid: TagId
+  name: str = field(compare=False)
+  description: str = field(compare=False)
+  category_uid: int = field(compare=False)
+  embedding: np.ndarray = field(compare=False,repr=False)
 
 
-class Anime(NamedTuple):
-  uid: int
-  name: str
-  genres_uid: List[int]
-  tags_uid: List[int]
-  tags_score: List[int]
-
-  def __eq__(self, anime: object) -> bool:
-    if not isinstance(anime, Anime):
-      return NotImplemented
-    return anime.uid == self.uid
-
-  def __hash__(self):
-    return hash(self.uid)
+@dataclass(init=True,repr=True,eq=True,order=False,frozen=True)
+class TagCategory:
+  uid: TagCategoryId
+  name: str = field(compare=False)
+  tags_uid: List[TagId] = field(compare=False)
 
 
-class Query(NamedTuple):
+@dataclass(init=True,repr=True,eq=True,order=False,frozen=True)
+class Anime:
+  uid: AnimeId
+  name: str = field(compare=False)
+  genres_uid: List[GenreId] = field(compare=False)
+  tags_uid: List[TagId] = field(compare=False)
+  tags_score: np.ndarray = field(compare=False)
+
+
+@dataclass(init=True,repr=True,eq=True,order=False,frozen=True)
+class Query:
   text: str
-  embedding: np.ndarray
+  embedding: Optional[np.ndarray] = field(compare=False)
 
 
-class SearchResult(NamedTuple):
-  query: Query
-  result_embeddings: np.ndarray
-  result_indexs: np.ndarray
-  scores: np.ndarray
-  anime_infos: List[Anime]
+@dataclass(init=True,repr=False,eq=True,order=True,frozen=True)
+class SearchResult:
+  query: Query = field(compare=False)
+  result_embeddings: np.ndarray = field(compare=False)
+  result_indexs: np.ndarray = field(compare=False)
+  scores: Scores = field(compare=True)
+  anime_infos: List[Anime] = field(compare=False)
+
+  def __getitem__(self,idx:int) -> Tuple[np.ndarray,int,int,Anime]:
+    return self.get_result(idx)
 
   def get_result(self,idx: int) -> Tuple[np.ndarray,int,int,Anime]:
-    return (self.result_embeddings[idx],self.result_indexs[idx],self.scores[idx],self.anime_infos[idx])
+    data = (self.result_embeddings,self.result_indexs,self.scores,self.anime_infos)
+    return compose(tuple,map)(lambda x: x[idx], data)
 
   @staticmethod
   def new_search_result(prev_result: "SearchResult", **kwargs) -> "SearchResult":
-    remaining_fields = set(prev_result._fields) - set(kwargs.keys())
+    remaining_fields = set(prev_result.__dict__.keys()) - set(kwargs.keys())
     kwargs.update({field_name: getattr(prev_result,field_name) for field_name in remaining_fields})
     return SearchResult(**kwargs)
 
 
-class SearchBase(NamedTuple):
-  MODEL: Model
-  INDEX : Any
-  ALL_ANIME: Dict[int,Anime]
-  ALL_TAGS: Dict[int,Tag]
-  ALL_TAGS_CATEGORY: Dict[int,TagCategory]
-  ALL_GENRE: Dict[int,Genre]
-  EMBEDDINGS: np.ndarray
-  LABELS: np.ndarray
-  TEXTS: List[str]
+@dataclass(init=True,repr=False,eq=False,order=False,frozen=True)
+class SearchData:
+  _embeddings: np.ndarray
+  _labels: np.ndarray
+  _texts: np.ndarray
+
+  def __getitem__(self,idx: int) -> Tuple[np.ndarray,AnimeId,str]:
+    data = (self._embeddings,self._labels,self._texts)
+    return compose(tuple,map)(lambda x: x[idx], data)
+
+
+@dataclass(init=True,repr=False,eq=False,order=False,frozen=True)
+class SearchBase:
+  model: Model
+  index : Any
+  search_data: SearchData
+  _animes: Dict[AnimeId,Anime]
+  _tags: Dict[TagId,Tag]
+  _tags_categories: Dict[TagCategoryId,TagCategory]
+  _genres: Dict[GenreId,Genre]
+
+  def get_tag(self,id:TagId) -> Tag:
+    return self._tags[id]
+
+  def get_tagcategory(self,id:TagCategoryId) -> TagCategory:
+    return self._tags_categories[id]
+
+  def get_genre(self,id:GenreId) -> Genre:
+    return self._genres[id]
+
+  def get_anime(self,id:AnimeId) -> Anime:
+    return self._animes[id]
+
+  def get_searchdata(self, idx) -> Tuple[np.ndarray,int,str]:
+    return self.search_data[idx]
 
 
 def sort_search(f):
-
   @wraps(f)
   def _impl(self, *args, **kwargs) -> SearchResult:
 
@@ -115,64 +130,56 @@ def sort_search(f):
 
     return SearchResult.new_search_result(search_result,result_embeddings=result_embeddings,
                                           result_indexs=result_indexs,anime_infos=anime_infos,scores=scores)
-
   return _impl
 
 
-def normalize(**kwargs):
-
-  def wrapper(f):
-
+def normalize(norm_f: Optional[Callable[[Scores],Scores]]=None):
+  def wrapper(f: Callable[[SearchResult],SearchResult]):
     @wraps(f)
     def _impl(self, *args) -> SearchResult:
 
       search_result = f(self,*args)
 
-      if kwargs.get("sigmoid") == True:
-        scores = utils.sigmoid(search_result.scores)
-      else:
-        scores = utils.rescale_scores(search_result.scores,
-                                      kwargs.get("t_min"),
-                                      kwargs.get("t_max"),
-                                      kwargs.get("inverse"))
+      scores = Maybe.from_optional(norm_f
+                ).bind_optional(
+                  lambda fn: fn(search_result.scores)
+                ).or_else_call(
+                  lambda: search_result.scores
+                )
+
       return SearchResult.new_search_result(search_result,scores=scores)
-
     return _impl
-
   return wrapper
 
 
-class ReIndexerBase(metaclass=ABCMeta):
-  def __init__(self,search_base:SearchBase, config:Config) -> None:
-    for name,val in zip(search_base._fields,search_base.__iter__()):
-      setattr(self,name,val)
+@dataclass
+class ReIndexerBase:
+  search_base: SearchBase
 
-    config_name = f"{self.name()}_config"
-    reindexer_config = getattr(config,config_name,None)
-    if reindexer_config is not None:
-      for name,val in zip(reindexer_config._fields,reindexer_config.__iter__()):
-        setattr(self,name,val)
-
-  @abstractmethod
   def __call__(self, search_result:SearchResult) -> SearchResult:
-    pass
-
-  @classmethod
-  def name(cls) -> str:
-    return cls.__name__.lower()
+    raise NotImplementedError
 
 
-class ReIndexingPipelineBase:
-  _reindexer_names: List[str] = []
+@dataclass
+class QueryProcessorBase:
+  def __call__(self, search_result:Query) -> Query:
+    raise NotImplementedError
 
-  def __call__(self, input:str) -> SearchResult:
-    return self.reindex_all(input)
 
-  def add_reindexer(self, name: str, reindexer: ReIndexerBase) -> None:
-    self._reindexer_names.append(name)
-    setattr(self,name,reindexer)
+@dataclass(init=True,repr=False,eq=False,order=False,frozen=True)
+class SearchPipelineBase(metaclass=ABCMeta):
+  query_processor_pipeline: List[Callable[[Query],Query]]
+  search: Callable[[Query],SearchResult]
+  reindexer_pipeline: List[Callable[[SearchResult],SearchResult]]
 
-  def reindex_all(self, input) -> SearchResult:
-    f = partial(getattr,self)
-    output = reduce(lambda input,name: f(name)(input),self._reindexer_names,input)
-    return output
+  def add_query_processor(self,f:Callable[[Query],Query]):
+    self.query_processor_pipeline.append(f)
+
+  def add_reindxer(self,f:Callable[[SearchResult],SearchResult]):
+    self.reindexer_pipeline.append(f)
+
+  def __call__(self, query:Query) -> SearchResult:
+    return reduce(lambda f,x: f(x),
+                  concat([self.query_processor_pipeline,[self.search],self.reindexer_pipeline]),
+                  query)
+
--- search/reindexers.py
+++ search/reindexers.py
@@ -1,137 +1,140 @@
-import operator
-from toolz.curried import compose, reduce, curry
 from typing import Union, Tuple
+from toolz.curried import compose, reduce, curry, flip# type: ignore
+from dataclasses import dataclass
+import operator
 
 from .config import *
 from .base_classes import *
 from .utils import rescale_scores
 
 
+@dataclass(init=True,frozen=True)
 class Search:
-  def __init__(self,search_base:SearchBase, config:Config):
+  search_base: SearchBase
+  embedding_dim: int
+  top_k: int
 
-    for name,val in zip(search_base._fields,search_base.__iter__()):
-      setattr(self,name,val)
+  @property
+  def model(self) -> Model:
+    return self.search_base.model
 
-    config_name = f"{self.name()}_config"
-    search_config = getattr(config,config_name)
+  @property
+  def knn_search(self) -> Any:
+    return self.search_base.index
 
-    for name,val in zip(search_config._fields,search_config.__iter__()):
-      setattr(self,name,val)
+  @property
+  def search_data(self) -> SearchData:
+    return self.search_base.search_data
 
-  def __call__(self, text: str) -> SearchResult:
-    return self.knn_search(text)
+  def new(self, search_base: SearchBase, config:SearchConfig) -> "Search":
+    return Search(search_base,config.embedding_dim,config.top_k)
 
-  @normalize(t_min=1,t_max=2,inverse=True)
   @sort_search
-  def knn_search(self, text:str) -> SearchResult:
-    q_embedding = self.MODEL(text)
-    q_embedding = np.expand_dims(q_embedding,0)
-    distances,n_id = self.INDEX.search(q_embedding,self.top_k)
+  def __call__(self, query: Query) -> SearchResult:
+    q_embedding = compose(
+                    flip(np.expand_dims,0),
+                    self.model)(query.text)
+
+    distances,n_id = self.knn_search(q_embedding,self.top_k)
     distances = 1/distances.squeeze()
     n_id = n_id.squeeze()
 
-    n_embeddings, n_anime_uids = self.EMBEDDINGS[n_id], self.LABELS[n_id]
-    n_anime = compose(list,map)(lambda uid: self.ALL_ANIME[uid], n_anime_uids)
-    query = Query(text,q_embedding.squeeze())
-
-    return SearchResult(query,n_embeddings,n_id,distances,list(n_anime))
-
-  @classmethod
-  def name(cls) -> str:
-    return cls.__name__.lower()
+    n_embeddings,n_anime_uids,texts = zip(*[self.search_data[idx] for idx in n_id])
+    n_animes = compose(list,map)(self.search_base.get_anime,n_anime_uids)
+    query = Query(query.text,q_embedding)
+    return SearchResult(query,n_embeddings,n_id,distances,n_animes)
 
 
-class TagReIndexer(ReIndexerBase):
+# class TagReIndexer(ReIndexerBase):
 
-  @normalize(t_min=1,t_max=8,inverse=False)
-  @sort_search
-  def __call__(self, search_result: SearchResult) -> SearchResult:
-    query_mat = self.tags_mat(search_result.query)
+#   @normalize(t_min=1,t_max=8,inverse=False)
+#   @sort_search
+#   def __call__(self, search_result: SearchResult) -> SearchResult:
+#     query_mat = self.tags_mat(search_result.query)
 
-    if self.tag_indexing_method == TagIndexing.per_category:
-      similarity_scores = compose(list,map)(self.per_category_indexing(query_mat),search_result.anime_infos)
-    elif self.tag_indexing_method == TagIndexing.all:
-      query_mat = query_mat.reshape(-1)
-      similarity_scores = compose(list,map)(self.all_category_indexing(query_mat),search_result.anime_infos)
-    else:
-      raise Exception(f"{self.tag_indexing_method} is not a corret type.")
+#     if self.tag_indexing_method == TagIndexing.per_category:
+#       similarity_scores = compose(list,map)(self.per_category_indexing(query_mat),search_result.anime_infos)
+#     elif self.tag_indexing_method == TagIndexing.all:
+#       query_mat = query_mat.reshape(-1)
+#       similarity_scores = compose(list,map)(self.all_category_indexing(query_mat),search_result.anime_infos)
+#     else:
+#       raise Exception(f"{self.tag_indexing_method} is not a corret type.")
 
-    similarity_scores = rescale_scores(similarity_scores,t_min=1,t_max=3,inverse=False)
-    similarity_scores *= anime_infos.scores
-    return SearchResult.new_search_result(search_result,scores=similarity_scores)
+#     similarity_scores = rescale_scores(similarity_scores,t_min=1,t_max=3,inverse=False)
+#     similarity_scores *= anime_infos.scores
+#     return SearchResult.new_search_result(search_result,scores=similarity_scores)
 
-  @staticmethod
-  def cosine_similarity(v1: np.ndarray,v2: np.ndarray) -> int:
-    return np.dot(v1, v2)/(np.linalg.norm(v1)*np.linalg.norm(v2))
+#   @staticmethod
+#   def cosine_similarity(v1: np.ndarray,v2: np.ndarray) -> int:
+#     return np.dot(v1, v2)/(np.linalg.norm(v1)*np.linalg.norm(v2))
 
-  def _get_tag_pos(self,tag_uid:int) -> Tuple[int,int]:
-    i = self.ALL_TAGS[tag_uid].category_uid
-    j = self.ALL_TAGS_CATEGORY[i].tag_pos(tag_uid)
-    return (i,j)
+#   def _get_tag_pos(self,tag_uid:int) -> Tuple[int,int]:
+#     i = self.ALL_TAGS[tag_uid].category_uid
+#     j = self.ALL_TAGS_CATEGORY[i].tag_pos(tag_uid)
+#     return (i,j)
 
-  def tags_mat(self, x:Union[Anime,Query]) -> np.ndarray:
-    len_tags_category = len(self.ALL_TAGS_CATEGORY.keys())
-    max_tags_uids = compose(max,map)(lambda val: len(val.tags_uid), self.ALL_TAGS_CATEGORY.values())
-    tags_mat = np.zeros((len_tags_category, max_tags_uids))
+#   def tags_mat(self, x:Union[Anime,Query]) -> np.ndarray:
+#     len_tags_category = len(self.ALL_TAGS_CATEGORY.keys())
+#     max_tags_uids = compose(max,map)(lambda val: len(val.tags_uid), self.ALL_TAGS_CATEGORY.values())
+#     tags_mat = np.zeros((len_tags_category, max_tags_uids))
 
-    def assign_score(uid,score):
-      pos = self._get_tag_pos(uid)
-      tags_mat[pos] = score
+#     def assign_score(uid,score):
+#       pos = self._get_tag_pos(uid)
+#       tags_mat[pos] = score
 
-    if isinstance(x,Anime):
-      for (uid,score) in zip(x.tags_uid,x.tags_score):
-        assign_score(uid,score)
-    elif isinstance(x,Query):
-      for uid in self.ALL_TAGS.keys():
-        score = self.cosine_similarity(self.ALL_TAGS[uid].embedding, x.embedding)
-        assign_score(uid,score)
-    else:
-      raise Exception(f"Only supported types are Anime and Query but {type(x)} is None of them")
-    return tags_mat
+#     if isinstance(x,Anime):
+#       for (uid,score) in zip(x.tags_uid,x.tags_score):
+#         assign_score(uid,score)
+#     elif isinstance(x,Query):
+#       for uid in self.ALL_TAGS.keys():
+#         score = self.cosine_similarity(self.ALL_TAGS[uid].embedding, x.embedding)
+#         assign_score(uid,score)
+#     else:
+#       raise Exception(f"Only supported types are Anime and Query but {type(x)} is None of them")
+#     return tags_mat
 
-  @curry
-  def per_category_indexing(self, query_mat: np.ndarray, anime_info: Anime) -> int:
-    anime_mat = self.tags_mat(anime_info)
-    x = compose(np.diag,np.dot)(anime_mat,query_mat.T)
-    y = compose(np.diag,np.dot)(anime_mat,anime_mat.T)
-    return np.dot(x,y)
+#   @curry
+#   def per_category_indexing(self, query_mat: np.ndarray, anime_info: Anime) -> int:
+#     anime_mat = self.tags_mat(anime_info)
+#     x = compose(np.diag,np.dot)(anime_mat,query_mat.T)
+#     y = compose(np.diag,np.dot)(anime_mat,anime_mat.T)
+#     return np.dot(x,y)
 
-  @curry
-  def all_category_indexing(self, query_mat: np.ndarray, anime_info: Anime) -> int:
-    anime_mat = self.tags_mat(anime_info)
-    anime_mat = anime_mat.reshape(-1)
-    return self.cosine_similarity(anime_mat,query_mat)
+#   @curry
+#   def all_category_indexing(self, query_mat: np.ndarray, anime_info: Anime) -> int:
+#     anime_mat = self.tags_mat(anime_info)
+#     anime_mat = anime_mat.reshape(-1)
+#     return self.cosine_similarity(anime_mat,query_mat)
 
 
-class AccReIndexer(ReIndexerBase):
+# class AccReIndexer(ReIndexerBase):
 
-  @normalize(t_min=1,t_max=6,inverse=False)
-  @sort_search
-  def __call__(self, search_result: SearchResult) -> SearchResult:
-    if self.acc_indexing_metric == AccIndexingMetric.add:
-      return self.acc_result(search_result,operator.add,initial_val=0)
-    elif self.acc_indexing_metric == AccIndexingMetric.mul:
-      return self.acc_result(search_result,operator.mul,initial_val=1)
-    else:
-      raise Exception("not correct type.")
+#   @normalize(t_min=1,t_max=6,inverse=False)
+#   @sort_search
+#   def __call__(self, search_result: SearchResult) -> SearchResult:
+#     if self.acc_indexing_metric == AccIndexingMetric.add:
+#       return self.acc_result(search_result,operator.add,initial_val=0)
+#     elif self.acc_indexing_metric == AccIndexingMetric.mul:
+#       return self.acc_result(search_result,operator.mul,initial_val=1)
+#     else:
+#       raise Exception("not correct type.")
 
-  def acc_result(self, search_result: SearchResult,
-      metric: Callable[[float,float],float], initial_val: float) -> SearchResult:
+#   def acc_result(self, search_result: SearchResult,
+#       metric: Callable[[float,float],float], initial_val: float) -> SearchResult:
 
-    where = lambda anime: [idx for idx, x in enumerate(search_result.anime_infos) if x == anime] #NOTE: can improve this?
+#     where = lambda anime: [idx for idx, x in enumerate(search_result.anime_infos) if x == anime] #NOTE: can improve this?
 
-    def acc(anime):
-      idxs = where(anime)
-      result_index = search_result.result_indexs[idxs[0]]
-      result_embedding = search_result.result_embeddings[idxs[0]]
-      score = reduce(metric,search_result.scores[idxs],initial_val)
-      return result_index,result_embedding,score
+#     def acc(anime):
+#       idxs = where(anime)
+#       result_index = search_result.result_indexs[idxs[0]]
+#       result_embedding = search_result.result_embeddings[idxs[0]]
+#       score = reduce(metric,search_result.scores[idxs],initial_val)
+#       return result_index,result_embedding,score
 
-    result = map(lambda anime: (anime,*acc(anime)), set(search_result.anime_infos))
+#     result = map(lambda anime: (anime,*acc(anime)), set(search_result.anime_infos))
 
-    anime_infos,result_indexs,result_embeddings,scores = zip(*result)
+#     anime_infos,result_indexs,result_embeddings,scores = zip(*result)
 
-    return SearchResult.new_search_result(search_result,anime_infos=list(anime_infos),result_indexs=np.array(result_indexs),
-                                          result_embeddings=np.array(result_embeddings),scores=np.array(scores))
+#     return SearchResult.new_search_result(search_result,anime_infos=list(anime_infos),result_indexs=np.array(result_indexs),
+#                                           result_embeddings=np.array(result_embeddings),scores=np.array(scores))
 
